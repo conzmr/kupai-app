758a4dd477c6a1edd4d86c2ba47f7f5bb07d5b97
Constanza Madrigal Reyes <madrigalreyes.c@gmail.com>
2019-10-06 23:09:38 -0500

    Login screen layout

---
+++ kupai/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  kupai
+//
+//  Created by Constanza Madrigal Reyes on 10/6/19.
+//  Copyright © 2019 Constanza Madrigal Reyes. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    var navigationTitle = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = navigationTitle
+    }
+    
+}
